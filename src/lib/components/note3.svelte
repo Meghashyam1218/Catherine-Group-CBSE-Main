2b1536bf20f44c66bcc8048d342cc8e55dd309cf
Chaitanya <Chaitanyadwarapudi123@gmail.com>
2022-11-22 15:20:56 +0530

    elements added

---
+++ src/lib/components/note3.svelte
@@ -0,0 +1,14 @@
+<section class="h-[90vh] pt-20">
+    <div class="flex flex-row">
+        <div class="pl-48 rounded-t-full">
+            <h1 class="text-3xl font-semibold pt-40">One ocean connects us</h1>
+            <p class="pt-10 text-2xl leading-10"><b>Ocean School's mission</b> is to help everyone understand <br> our influence on the ocean and the ocean’s influence on <br> us. We aim to empower people with knowledge, tools, and <br> inspiration so that they can take action for the ocean.</p>
+            <div class="pt-10">
+                <button class="bg-[#001253] hover:bg-white hover:text-black ease-in-out duration-500 rounded-[40px] w-52 h-14 text-white	text-lg">Partner with us</button>
+            </div>
+        </div>
+        <div class="pl-60 ">
+            <img src="./4.png" class="rounded-[150px] w-[700px]" alt="">
+        </div>
+        </div>
+</section>
---
+++ src/lib/components/note4.svelte
@@ -0,0 +1,11 @@
+<section class="h-[90vh] pt-20">
+    <div class="flex flex-row">
+        <div class="pl-48 rounded-t-full">
+            <h1 class="text-3xl font-semibold pt-40">Maximize your class time</h1>
+            <p class="pt-10 text-2xl leading-10">We've worked with <b>teachers like you</b> to offer one-of-a- <br> kind learning experiences. Choose one of our themed <br> collections, or mix and match resources from our <br> <b>searchable catalogue.</b> Our activities are <b>Creative</b><br> <b>Commons,</b>so you can remix them to suit your needs!</p>
+        </div>
+        <div class="pl-60 ">
+            <img src="./5.png" class="rounded-[150px] w-[700px]" alt="">
+        </div>
+        </div>
+</section>
