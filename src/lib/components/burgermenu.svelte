9f5c561ce2dffc484ac316d7c39f2340954a7426
Chaitanya <Chaitanyadwarapudi123@gmail.com>
2022-11-27 12:06:09 +0530

    added elements

---
+++ src/lib/components/burgermenu.svelte
@@ -0,0 +1,62 @@
+<section class="overflow-hidden">
+    <div class="bg-white rounded-2xl h-[90vh]">
+        <div>
+            <div class="flex">
+                <p class="pl-24 pt-7 text-2xl text-black mb-8">Close</p>
+        <ul class="flex ml-[1300px] gap-11 pt-6 text-xl text-black">
+            <a href="#"><li>Educational Resources</li></a>
+            <a href="#"><li>Community</li></a>
+            <a href="#"><li>Fr</li></a>
+            </ul>
+            </div>
+            <hr class="pt-9" width="500%">
+            <div  class="flex">
+                <div class="pl-36 text-black text-2xl">
+                    <h2 class="pl-10">Browse our content</h2>
+                    <ul class="pl-10">
+                        <h5 class="pt-5 font-extrabold mb-10 mt-10">BY GRADE</h5>
+                        <a href="#"><li id="i1" class="mb-4 hover:underline underline-offset-4">5-6</li></a>
+                        <a href="#"><li class="mb-4 hover:underline underline-offset-4">7-8</li></a>
+                        <a href="#"><li class="mb-4 hover:underline underline-offset-4">9+</li></a>
+                        <h5 class="pt-5 font-extrabold mb-10">BY TIME</h5>
+                        <a href="#"><li class="mb-4 hover:underline underline-offset-4">15 min</li></a>
+                        <a href="#"><li class="mb-4 hover:underline underline-offset-4">30 min</li></a>
+                        <a href="#"><li class="mb-4 hover:underline underline-offset-4">45 min</li></a>
+                    </ul>
+                </div>
+                <div class="pl-40 text-black ml-12 text-2xl">
+                    <h5 class="pt-5 font-extrabold mt-[74px]">BY SUBJECT</h5>
+                    <ul class="mt-10">
+                    <a href="#"><li class="mb-4 hover:underline underline-offset-4">Biology</li></a>
+                    <a href="#"><li class="mb-4 hover:underline underline-offset-4">Career education</li></a>
+                    <a href="#"><li class="mb-4 hover:underline underline-offset-4">Indigenous studies</li></a>
+                    <a href="#"><li class="mb-4 hover:underline underline-offset-4">Geography</li></a>
+                    <a href="#"><li class="mb-4 hover:underline underline-offset-4">Mathematics</li></a>
+                    <a href="#"><li class="mb-4 hover:underline underline-offset-4">Digital media</li></a>
+                    <a href="#"><li class="mb-4 hover:underline underline-offset-4">Science</li></a>
+                    <a href="#"><li class="mb-4 hover:underline underline-offset-4">Social studies</li></a>
+                    </ul>
+                </div>
+                <div>
+                    <button class="ml-52 h-40 w-72 rounded-[30px] bg-gradient-to-r from-purple-500 to-pink-500">Button</button>
+                </div>
+            </div>
+            <div>
+                <button class="mt-9 ml-52 bg-slate-400 w-44 h-12 rounded-full hover:bg-slate-900 hover:text-white ease-in-out duration-500">Browse All Content</button>
+            </div>
+        </div>
+    </div>
+</section>
+<style>
+
+li::before{
+    content: "";
+    width: 20px;
+    color: white;
+    height: 3px;
+
+}
+
+
+
+</style>
