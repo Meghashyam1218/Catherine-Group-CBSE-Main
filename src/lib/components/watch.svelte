743e4759f97edc0f38f130ed9f96336424169026
Chaitanya <Chaitanyadwarapudi123@gmail.com>
2022-11-30 10:31:27 +0530

    added elements

--- src/lib/components/watch.svelte
+++ src/lib/components/watch.svelte
@@ -1,17 +1,12 @@
-<section class="h-[90vh] bg-[#eff3f3] relative w-[100%]">
-	<div
-		class="absolute grid blur-sm place-items-center bg-cover rounded-[30px] h-[100%] w-[100%]"
-		style="background-image: url(./water.jpeg);"
-	/>
-	<div class="absolute flex flex-col justify-center items-center h-[100%] w-[100%]">
+<section class="h-[90vh] bg-[#EFF3F3] m-6 relative rounded-[30px]">
+	<div class="absolute grid place-items-center bg-cover rounded-[30px] h-[100%] w-[100%]" style="background-image: url(./water.jpeg);"></div>
+	<div class="h-[90vh] backdrop-blur-lg rounded-[30px] ">
+	<div class="absolute flex flex-col justify-center rounded-[30px] items-center h-[100%] w-[100%]">
 		<div class="pb-10">
 			<span class="font-extrabold text-9xl text-white place-self-center">WATCH TRAILER</span>
 		</div>
-		<div
-			class=" shadow-2xl rounded-[100px] bg-cover w-[50vw] h-[50vh]"
-			style="background-image: url(./water.jpeg);"
-		>
+		<div class=" shadow-2xl rounded-[100px] bg-cover w-[50vw] h-[50vh]" style="background-image: url(./water.jpeg);">
 			<img src="./turtle.png" alt="" />
 		</div>
-	</div>
+	</div></div>
 </section>
