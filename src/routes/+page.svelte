f270dae21d4f3cfe29e1d553a8b17075923b2531
Meghashyam1218 <gurrammeghashyam@outlook.com>
2022-12-07 01:37:55 +0530

    highlights

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@ import Hero from "$lib/components/hero.svelte";
 import Navbar from "$lib/components/navbar.svelte";
 import Why from "$lib/components/why.svelte";
 import Cards from "$lib/components/cards.svelte"
+import Highlights from "$lib/components/highlights.svelte";
 
 </script>
 
@@ -12,6 +13,7 @@ import Cards from "$lib/components/cards.svelte"
 <div class="">
     <Hero/>
     <Why/>
+    <Highlights/>
     <Cards/>
 
 </div>
