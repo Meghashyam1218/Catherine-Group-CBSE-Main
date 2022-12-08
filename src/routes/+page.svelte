2d6d36b95dfe8f5acb17d2d669dddf825bf4126c
ANCIENTINSANE <107138522+ANCIENTINSANE@users.noreply.github.com>
2022-12-08 14:55:52 +0530

    Revert "nav and hero"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,20 @@
-
 <script>
-	
-	import { onMount } from 'svelte';
-	import Hero from '$lib/components/hero.svelte';
-	import Navbar from '$lib/components/navbar.svelte';
-	import Why from '$lib/components/why.svelte';
-	import Cards from '$lib/components/cards.svelte';
-	import Highlights from '$lib/components/highlights.svelte';
+
+import Hero from "$lib/components/hero.svelte";
+import Navbar from "$lib/components/navbar.svelte";
+import Why from "$lib/components/why.svelte";
+import Cards from "$lib/components/cards.svelte"
+import Highlights from "$lib/components/highlights.svelte";
+
 </script>
 
+
 <section class="">
-	<div class="">
-		<Navbar />
-		<Hero />
-		<Why />
-		<Highlights />
-		<Cards />
-	</div>
+<div class="">
+    <Hero/>
+    <Why/>
+    <Highlights/>
+    <Cards/>
+
+</div>
 </section>
