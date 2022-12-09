267d4882dc0723823cff047ff7afeb431b3ae350
Rahul1342002 <trahul13402@gmail.com>
2022-12-09 16:16:58 +0530

    change in about us

---
+++ src/routes/aboutus/components/managments.svelte
@@ -0,0 +1,18 @@
+<section class="h-full ">
+    <div class="lg:w-[80vw] flex ml-auto mr-auto p-8 lg:p-20 flex-col  md:flex-row text-justify md:text-start items-center justify-center">
+       <div class="basis-1/2 ">
+           <img src="/aboutus.webp" alt="" class="rounded-[25%] sticky ">
+       </div>
+       <div class="basis-1/2 text-neutral-800 flex flex-col  justify-center md:pl-14 xl:pr-0 my-6 md:my-0">
+           <h1 class="text-3xl font-semibold text-center md:text-start "> Managamenet</h1>
+           <br>
+           <p class="md:text-xl text-base">Its Director Dr. Sushma Evangeline Rayi, MCA., M.Ed., PhD (Education), has rich exposure and experience in circular and cocurricular aspects in the field od education.
+           </p>
+           <br>
+           <p class="md:text-xl text-base">
+            Its Secratary & Correspondent Mr. Oliver rayi, is an Education Technologist, Value Education Specialist, Lifeskills Expert and also holds a Masters in Business Adminisytration. He was awarded the Leadership Award for Education Excellence in 2014.He was instrumental in ushering in innovation in teaching menthods, His leadership ensures thatschools as an organization is actively ;listening, reflecting, and eveloving. Mr. Oliver Rayi ensured the provison of International School facilities at anormal school fee.  </p>
+       </div>
+       
+    </div>
+</section>
+   
