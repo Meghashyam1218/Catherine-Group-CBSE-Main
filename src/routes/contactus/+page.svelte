e1f94e9d64a62504c598c533934d2b4ec1da61be
Rahul1342002 <trahul13402@gmail.com>
2022-12-10 20:38:06 +0530

    contact us added

---
+++ src/routes/contactus/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Form from "./components/form.svelte";
+    import Top from "$lib/components/top.svelte";
+    import Footer from "$lib/components/footer.svelte";
+</script>
+
+<Top/>
+<Form/>
+<Footer/>
---
+++ src/routes/contactus/components/form.svelte
@@ -0,0 +1,237 @@
+<!-- Container for demo purpose -->
+<div class="container my-24 px-6 mx-auto">
+	<!-- Section: Design Block -->
+	<section class="mb-32 text-gray-800">
+		<div class="flex justify-center">
+			<div class="text-center lg:max-w-3xl md:max-w-xl">
+				<h2 class="text-6xl font-bold mb-12 px-6">Contact us</h2>
+			</div>
+		</div>
+
+		<div class="flex flex-wrap">
+			<div class="grow-0 shrink-0 basis-auto mb-12 lg:mb-0 w-full lg:w-5/12 px-3 lg:px-6">
+				<form>
+					<div class="form-group mb-6">
+						<input
+							type="text"
+							class="form-control block
+                  w-full
+                  px-3
+                  py-1.5
+                  text-base
+                  font-normal
+                  text-gray-700
+                  bg-white bg-clip-padding
+                  border border-solid border-gray-300
+                  rounded
+                  transition
+                  ease-in-out
+                  m-0
+                  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+							id="exampleInput7"
+							placeholder="Name"
+						/>
+					</div>
+					<div class="form-group mb-6">
+						<input
+							type="email"
+							class="form-control block
+                  w-full
+                  px-3
+                  py-1.5
+                  text-base
+                  font-normal
+                  text-gray-700
+                  bg-white bg-clip-padding
+                  border border-solid border-gray-300
+                  rounded
+                  transition
+                  ease-in-out
+                  m-0
+                  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+							id="exampleInput8"
+							placeholder="Email address"
+						/>
+					</div>
+					<div class="form-group mb-6">
+						<textarea
+							class="
+                  form-control
+                  block
+                  w-full
+                  px-3
+                  py-1.5
+                  text-base
+                  font-normal
+                  text-gray-700
+                  bg-white bg-clip-padding
+                  border border-solid border-gray-300
+                  rounded
+                  transition
+                  ease-in-out
+                  m-0
+                  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+                "
+							id="exampleFormControlTextarea13"
+							rows="3"
+							placeholder="Message"
+						/>
+					</div>
+					<div class="form-group form-check text-center mb-6">
+						<input
+							type="checkbox"
+							class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain mr-2 cursor-pointer"
+							id="exampleCheck87"
+							checked
+						/>
+						<label class="form-check-label inline-block text-gray-800" for="exampleCheck87"
+							>Send me a copy of this message</label
+						>
+					</div>
+					<button
+						type="submit"
+						class="
+                w-full
+                px-6
+                py-2.5
+                bg-blue-600
+                text-white
+                font-medium
+                text-xs
+                leading-tight
+                uppercase
+                rounded
+                shadow-md
+                hover:bg-blue-700 hover:shadow-lg
+                focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+                active:bg-blue-800 active:shadow-lg
+                transition
+                duration-150
+                ease-in-out">Send</button
+					>
+				</form>
+			</div>
+			<div class="grow-0 shrink-0 basis-auto w-full lg:w-7/12">
+				<div class="flex flex-wrap">
+					<div class="mb-12 grow-0 shrink-0 basis-auto w-full lg:w-6/12 px-3 lg:px-6">
+						<div class="flex items-start">
+							<div class="shrink-0">
+								<div
+									class="p-4 bg-blue-600 rounded-md shadow-xl w-14 h-14 flex items-center justify-center"
+								>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke-width="1.5"
+										stroke="white"
+										class="w-6 h-6"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+										/>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+										/>
+									</svg>
+								</div>
+							</div>
+							<div class="grow ml-6">
+								<p class="font-bold mb-1">Address</p>
+								<p class="text-gray-500">Vidyamarg, Bheemunipatnam,</p>
+								<p class="text-gray-500">Visakhpatnam, PIN- 531163</p>
+							</div>
+						</div>
+					</div>
+					<div class="mb-12 grow-0 shrink-0 basis-auto w-full lg:w-6/12 px-3 lg:px-6">
+                                                <div class="flex align-start">
+							<div class="shrink-0">
+								<div
+									class="p-4 bg-blue-600 rounded-md shadow-md w-14 h-14 flex items-center justify-center"
+								>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke-width="1.5"
+										stroke="white"
+										class="w-6 h-6"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+										/>
+									</svg>
+								</div>
+							</div>
+							<div class="grow ml-6">
+								<p class="font-bold mb-1">Email Address</p>
+								<p class="text-gray-500">catherinebheemili@gmail.com</p>
+								
+							</div>
+						</div>
+						
+					</div>
+					<div class="mb-12 grow-0 shrink-0 basis-auto w-full lg:w-6/12 px-3 lg:px-6">
+                                                <div class="flex items-start">
+							<div class="shrink-0">
+								<div
+									class="p-4 bg-blue-600 rounded-md shadow-md w-14 h-14 flex items-center justify-center"
+								>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke-width="1.5"
+										stroke="white"
+										class="w-6 h-6"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z"
+										/>
+									</svg>
+								</div>
+							</div>
+							<div class="grow ml-6">
+								<p class="font-bold mb-1">Contact Number</p>
+								<p class="text-gray-500">+91 99887 99887</p>
+							</div>
+						</div>
+                                                
+						
+					</div>
+					<div class="mb-12 grow-0 shrink-0 basis-auto w-full lg:w-6/12 px-3 lg:px-6">
+                                                <div class="flex align-start">
+							<div class="shrink-0">
+								<div
+									class="p-4 bg-blue-600 rounded-md shadow-md w-14 h-14 flex items-center justify-center text-white"
+								>
+                                                                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                                                                width="50" height="50"
+                                                                viewBox="0 0 50 50"
+                                                                stroke="white"
+                                                                fill="white">
+                                                                <path d="M 30.140625 2 C 26.870375 2 24.045399 2.9969388 22.0625 4.9667969 C 20.079601 6.936655 19 9.823825 19 13.367188 L 19 18 L 13 18 A 1.0001 1.0001 0 0 0 12 19 L 12 27 A 1.0001 1.0001 0 0 0 13 28 L 19 28 L 19 47 A 1.0001 1.0001 0 0 0 20 48 L 28 48 A 1.0001 1.0001 0 0 0 29 47 L 29 28 L 36 28 A 1.0001 1.0001 0 0 0 36.992188 27.125 L 37.992188 19.125 A 1.0001 1.0001 0 0 0 37 18 L 29 18 L 29 14 C 29 12.883334 29.883334 12 31 12 L 37 12 A 1.0001 1.0001 0 0 0 38 11 L 38 3.3457031 A 1.0001 1.0001 0 0 0 37.130859 2.3554688 C 36.247185 2.2382213 33.057174 2 30.140625 2 z M 30.140625 4 C 32.578477 4 34.935105 4.195047 36 4.2949219 L 36 10 L 31 10 C 28.802666 10 27 11.802666 27 14 L 27 19 A 1.0001 1.0001 0 0 0 28 20 L 35.867188 20 L 35.117188 26 L 28 26 A 1.0001 1.0001 0 0 0 27 27 L 27 46 L 21 46 L 21 27 A 1.0001 1.0001 0 0 0 20 26 L 14 26 L 14 20 L 20 20 A 1.0001 1.0001 0 0 0 21 19 L 21 13.367188 C 21 10.22255 21.920305 7.9269075 23.472656 6.3847656 C 25.025007 4.8426237 27.269875 4 30.140625 4 z"></path>
+                                                                </svg>
+									
+								</div>
+							</div>
+							<div class="grow ml-6">
+								<p class="font-bold mb-1">Facebook</p>
+								<p class="text-gray-500"><a href="https://www.facebook.com/catherinebheemili/">catherinebheemili</a></p>
+                                                        
+							</div>
+						</div>  
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
