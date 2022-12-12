d4de5942e825d3f28867c2a7ff5441f948af9845
ANCIENTINSANE <surendrakumar246810.bits@gmail.com>
2022-12-12 15:06:38 +0530

    hello

--- src/lib/components/why.svelte
+++ src/lib/components/why.svelte
@@ -3,7 +3,7 @@
         <div class="flex flex-col xl:w-[50vw]">
             <div><p class="xl:text-3xl 3xs:text-2xl font-semibold">What is Catherine?</p></div>
             <div class="mt-[5%] xl:mr-[10%]"><p class="xl:text-xl text-base text-justify leading-8">Catherine Schools was founded in 1983 by R.D.Ratna Raju and Lt. Prof (Mrs) Satya Bala. This Christian Minority Institution educates,nurtures and inspires girls and boys of diverse backgrounds to acheive their highest academic and creative potential, to embrase ethical values, and to become active cintributors to their communities.</p></div>
-            <div class="mt-[5%] 3xs:grid 3xs:self-center xl:self-start"><button class="bg-[#0c1637] text-white xl:text-xl text-base font-semibold p-2 xl:p-4 rounded-full duration-700 hover:bg-white hover:text-[#0c1637]">Explore Our Resources</button></div>
+            <a href="/aboutus" class="mt-[5%] 3xs:grid 3xs:self-center xl:self-start"><button class="bg-[#0c1637] text-white xl:text-xl text-base font-semibold p-2 xl:p-4 rounded-full duration-700 hover:bg-white hover:text-[#0c1637]">Explore more</button></a>
         </div>
         <div class="flex flex-col xl:w-[40vw] pt-[5%] xl:pl-[5%] space-y-6">
             <div><p class="xl:text-xl text-base text-justify leading-8">The founders both being retired professors have been serving the cause of education for more than 45 years. they dram from the rich exposure to education system in Europe, America and Asia and the amazing story of Catherine School, which has already established a legacy of academic excellence, is a story that continues unfold</p></div>
