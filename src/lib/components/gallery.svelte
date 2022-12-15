49ddd41c4b3291cf9f7cc5a57bb7eb992519cd19
Meghashyam1218 <gurrammeghashyam@outlook.com>
2022-12-15 20:35:38 +0530

    change gallery of home

--- src/lib/components/gallery.svelte
+++ src/lib/components/gallery.svelte
@@ -91,25 +91,25 @@
         </div>
         <div class="w-1/2 p-1 md:p-2 hover:brightness-[.7] cursor-pointer transition-all ease-linear">
           <img alt="gallery" class="block img-home object-cover object-center w-full h-full rounded-lg"
-            src="./gallery/img (2).jpeg">
+            src="./gallery/img (3).jpeg">
         </div>
         <div class="w-full p-1 md:p-2 hover:brightness-[.7] cursor-pointer transition-all ease-linear">
           <img alt="gallery" class="block img-home object-cover object-center w-full h-full rounded-lg"
-            src="./gallery/img (3).jpeg">
+            src="./gallery/img (5).jpeg">
         </div>
       </div>
       <div class="flex flex-wrap md:w-1/2">
         <div class="w-full p-1 md:p-2 hover:brightness-[.7] cursor-pointer transition-all ease-linear">
           <img alt="gallery" class="block img-home object-cover object-center w-full h-full rounded-lg"
-            src="./gallery/img (5).jpeg">
+            src="./gallery/img (7).jpeg">
         </div>
         <div class="w-1/2 p-1 md:p-2 hover:brightness-[.7] cursor-pointer transition-all ease-linear">
           <img alt="gallery" class="block img-home object-cover object-center w-full h-full rounded-lg"
-            src="./gallery/img (6).jpeg">
+            src="./gallery/img (9).jpeg">
         </div>
         <div class="w-1/2 p-1 md:p-2 hover:brightness-[.7] cursor-pointer transition-all ease-linear">
           <img alt="gallery" class="block img-home object-cover object-center w-full h-full rounded-lg"
-            src="./gallery/img (4).jpeg">
+            src="./gallery/img (11).jpeg">
         </div>
       </div>
     </div>
