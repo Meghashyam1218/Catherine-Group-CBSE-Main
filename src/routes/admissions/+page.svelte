ff555e65f08d99515a96b93c1c6ae82ceccd8f21
Rahul1342002 <trahul13402@gmail.com>
2022-12-15 10:31:38 +0530

    admission & faculty

---
+++ src/routes/admissions/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Top from "$lib/components/top.svelte";
+    
+    import Footer from "$lib/components/footer.svelte";
+	
+	import Admissions from "./components/admissions.svelte";
+	import Steps from "./components/steps.svelte";
+</script>
+
+<Top name="Admission" heading="Admission" bgImage="./top-1.jpg"/>
+
+<Admissions/>
+<Steps/>
+<Footer/>
---
+++ src/routes/educators/components/educators.svelte
@@ -0,0 +1,15 @@
+<section class="h-full my-10">
+    <div class="lg:max-w-[1100px] xl:max-w-[80vw] flex ml-auto mr-auto  flex-col-reverse  lg:flex-row text-justify  items-center justify-center">
+        <div data-aos="fade-up" data-aos-once="true" class="basis-1/2 text-neutral-800 flex flex-col  justify-center p-4 m-4">
+        <h1 class="text-3xl font-semibold text-center md:text-start "> Educators</h1>
+        <br>
+        <p class="md:text-xl text-base">A diverse and talented group of individuals, our faculty and staff bring their passions to the classrooms and the entire school environment. Science, Mathematics, Languages or Sports they all share a positive dedication to education and a deep commitment to seeing every Catherine student flourish. </p>
+        <br>
+        <p class="md:text-xl text-base">A student's style of learning varies, so it is imperative to have faculties that understand and can recognize a student's response to inputs. Our solemnized teaching method ensures our educational values are promoted and followed. But more importantly, it is the value and faith we place on our faculties that in turn ensure our method is applied seamlessly.</p>
+        
+        </div>
+    <div data-aos="zoom-in" data-aos-once="true" class="basis-1/2 p-4 m-4 md:max-w-[600px] ">
+        <img src="/educators.jpg" alt="" class="rounded-[25%] sticky ">
+    </div>
+ </div>
+</section>  
